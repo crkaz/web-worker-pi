bb9e31d62a40ce266b343645005c9d510b605e03
CrkazDesktopNVME <zakcatherall@outlook.com>
2021-02-17 01:08:54 +0000

    Added keeping of best estimation and fixed comment format in ui.js

--- public/js/ui.js
+++ public/js/ui.js
@@ -7,11 +7,13 @@ const CANVAS = document.getElementById("piCanvas");
 const CTX = CANVAS.getContext("2d");
 const WIDTH = 800;
 const HALF_WIDTH = 400;
+const PI = Math.PI;
 const TWO_PI = Math.PI * 2;
 const TOTAL_HITS = document.getElementById("total-hit-count");
 const RED_HITS = document.getElementById("red-hit-count");
 const BLUE_HITS = document.getElementById("blue-hit-count");
 const PI_ESTIMATION = document.getElementById("pi-estimation");
+const PI_BEST_ESTIMATION = document.getElementById("pi-best-estimation");
 const START_BUTTON = document.getElementById("start-button");
 const STOP_BUTTON = document.getElementById("stop-button");
 const TEXT_LOG = document.getElementById("text-log");
@@ -22,6 +24,7 @@ const BLACK = "#000000";
 // #endregion
 
 let loggingEnabled = true;
+let bestEstimation = 0;
 
 class MyUI {
 
@@ -31,7 +34,7 @@ class MyUI {
         this.reset();
     }
 
-    // Reset all UI elements to default state and/or value.
+    /** Reset all UI elements to default state and/or value. */
     reset() {
         CTX.fillStyle = WHITE;
         CTX.fillRect(-HALF_WIDTH, -HALF_WIDTH, WIDTH, WIDTH);
@@ -40,28 +43,35 @@ class MyUI {
         TEXT_LOG.innerText = "";
     }
 
-    // Toggle start/stop buttons.
+    /** Toggle start/stop buttons. */
     toggleUiState() {
         START_BUTTON.disabled = !START_BUTTON.disabled;
         STOP_BUTTON.disabled = !STOP_BUTTON.disabled;
     }
 
-    // Draw on Canvas at coordinates in postMessage data.
+    /** Draw on Canvas at coordinates in postMessage data. */
     updateCanvas(data) {
         CTX.fillStyle = data.colour;
         CTX.fillRect(data.y, data.x, 1, 1);
     }
 
-    // Update counters with values in postMessage data.
+    /** Update counters with values in postMessage data. */
     updateCounters(data) {
         TOTAL_HITS.innerText = data.totalHits;
         RED_HITS.innerText = data.redHits;
         BLUE_HITS.innerText = data.blueHits;
         PI_ESTIMATION.innerText = data.estimation;
+        this.updateBestEstimation(data.estimation);
         this.log(data.estimation + " (" + data.deltaTime + "ms)");
     }
 
-    // Toggle a primitive log of PI estimations.
+    /** Calculate if the the latest estimation is the closest to actual PI yet. */
+    updateBestEstimation(estimation) {
+        Math.abs(PI - estimation) < Math.abs(PI - bestEstimation) ? bestEstimation = estimation : null;
+        PI_BEST_ESTIMATION.innerText = "Best: " + bestEstimation; 
+    }
+
+    /** Toggle a primitive log of PI estimations. */
     toggleLogging(enabled) {
         !enabled ? TEXT_LOG.innerText = "" : null; 
         loggingEnabled = enabled;
@@ -69,7 +79,7 @@ class MyUI {
 
     // ---
 
-    // Part of Canvas setup.
+    /** Part of Canvas setup. */
     drawCircle() { 
         CTX.fillStyle = BLACK;
         CTX.beginPath();
@@ -77,7 +87,7 @@ class MyUI {
         CTX.stroke();
     }
 
-    // Write text to a the UI 'log'.
+    /** Write text to a the UI 'log'. */
     log(entry) {
         loggingEnabled ? TEXT_LOG.innerText = entry + NEW_LINE + TEXT_LOG.innerText : TEXT_LOG.innerText = EMPTY;
     }
